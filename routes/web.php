27eb758c03152176329c642a3c6d17457350a8fc
ziafadhilah <ziafadhilah98@gmail.com>
2025-01-12 13:43:17 +0700

    edit delete category & cars done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ReturnedController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,6 +14,9 @@ Route::prefix('/category')->name('category.')->group(function () {
     Route::get('/', [CategoryController::class, 'index'])->name('index');
     Route::get('/create', [CategoryController::class, 'create'])->name('create');
     Route::post('/', [CategoryController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [CategoryController::class, 'edit'])->name('edit');
+    Route::put('/{id}', [CategoryController::class, 'update'])->name('update');
+    Route::delete('/{id}', [CategoryController::class, 'destroy'])->name('destroy');
 });
 
 // Cars
@@ -20,4 +24,14 @@ Route::prefix('/cars')->name('cars.')->group(function () {
     Route::get('/', [CarController::class, 'index'])->name('index');
     Route::get('/create', [CarController::class, 'create'])->name('create');
     Route::post('/', [CarController::class, 'store'])->name('store');
+    Route::get('/edit/{id}', [CarController::class, 'edit'])->name('edit');
+    Route::put('/{id}', [CarController::class, 'update'])->name('update');
+    Route::delete('/{id}', [CarController::class, 'destroy'])->name('destroy');
+});
+
+// Returned
+Route::prefix('/returned')->name('returned.')->group(function () {
+    Route::get('/', [ReturnedController::class, 'index'])->name('index');
+    Route::get('/create', [ReturnedController::class, 'create'])->name('create');
+    Route::post('/', [ReturnedController::class, 'store'])->name('store');
 });
